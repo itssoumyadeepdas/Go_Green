e9f75c4e6bd99bf5de248393e0d07ea1f1476752
Sagar Kumar <74974420+sagarkumar3105@users.noreply.github.com>
2021-10-24 00:35:44 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df=pd.read_csv("cred.csv")
+def checkUsername(un):
+    for x in df['Username']:
+        if un==x:
+            con=True
+            break
+        else:
+            con=False
+    return con
+
+def checkPassword(p):
+    for x in df['Password']:
+        if p==x:
+            con=True
+            break
+        else:
+            con=False
+    return con
+    
