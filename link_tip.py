398efbdee876d13f8aaa52d4c6007dc0fc55da5f
Sagar Kumar <74974420+sagarkumar3105@users.noreply.github.com>
2021-10-24 01:59:02 +0530

    Update link_tip.py

--- link_tip.py
+++ link_tip.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-t=pd.read_csv("Resource/tips.csv")
-v=pd.read_csv("Resource/link.csv")
+t=pd.read_csv("https://github.com/sagarkumar3105/Go_Green/blob/2b413d4d7e78cab16d38b5a50cc51e9b753bb6c0/Resource/tips.csv")
+v=pd.read_csv("https://github.com/sagarkumar3105/Go_Green/blob/907dd8eefaf8e46eb4c5b830fda5b807cdf6154c/Resource/link.csv")
 def getLink(item):
     d=v.loc[v['Item']==item]
     return (d.iloc[0,1])
