24ea280c1c3d5ec2d4862b99e6d42f87518b915a
Sagar Kumar <74974420+sagarkumar3105@users.noreply.github.com>
2021-10-24 02:30:27 +0530

    Update link_tip.py

--- link_tip.py
+++ link_tip.py
@@ -1,10 +1,8 @@
 import pandas as pd
-from pathlib import path
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Resource'))
-import Resource.link
+import os
+tfile=os.path.abspath("https://github.com/sagarkumar3105/Go_Green/blob/907dd8eefaf8e46eb4c5b830fda5b807cdf6154c/Resource/tips.csv")
+vfile=os.path.abspath("https://github.com/sagarkumar3105/Go_Green/blob/907dd8eefaf8e46eb4c5b830fda5b807cdf6154c/Resource/link.csv")
 t=pd.read_csv("tfile")
-v=pd.read_csv("https://github.com/sagarkumar3105/Go_Green/blob/907dd8eefaf8e46eb4c5b830fda5b807cdf6154c/Resource/link.csv")
 def getLink(item):
     d=v.loc[v['Item']==item]
     return (d.iloc[0,1])
